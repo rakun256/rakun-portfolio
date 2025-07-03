56e5ff28699a5c559278c5470aeca901c8f6751b
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2025-07-04 01:55:38 +0300

    Implement code changes to enhance functionality and improve performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,37 @@ import MatrixBackground from '@/components/layout/MatrixBackground'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Emre Uslu – Portfolio',
-  description: 'Frontend & Mobile Developer',
-}
+  title: "Emre Uslu – Fullstack & Mobile Developer",
+  description: "Portfolio of Emre Uslu, passionate frontend and mobile developer.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  metadataBase: new URL("https://emreuslu.terapinisec.com"), 
+  openGraph: {
+    title: "Emre Uslu – Fullstack & Mobile Developer",
+    description: "Portfolio of Emre Uslu, passionate frontend and mobile developer.",
+    url: "https://emreuslu.terapinisec.com",
+    siteName: "Emre Uslu Portfolio",
+    images: [
+      {
+        url: "/og-image.png", // public klasörüne koy
+        width: 1200,
+        height: 630,
+        alt: "Emre Uslu Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Emre Uslu – Fullstack & Mobile Developer",
+    description: "Portfolio of Emre Uslu, passionate frontend and mobile developer.",
+    images: ["/og-image.png"],
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
