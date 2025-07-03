a38a37e26fc1b97d3dd944e0b9dfcf3cc77bd3ba
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2025-07-04 01:45:23 +0300

    Update about.ts to enhance the description with additional context and clarity

--- data/about.ts
+++ data/about.ts
@@ -5,7 +5,9 @@ const about = {
   title: "Fullstack & Mobile Developer",
   description: `
     I am a passionate and practical engineering student with hands-on experience in both mobile and web development.
+    
     I've worked on impactful projects with organizations like Turkcell, Dogus Teknoloji and academic communities.
+    
     I specialize in frontend technologies and am expanding into backend development to deliver full-scale solutions using React, Java, and Spring Boot.
   `,
   highlights: [
